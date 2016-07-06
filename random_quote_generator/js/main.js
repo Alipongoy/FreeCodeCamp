e6bf47d8ba42745ee1dba7b5e7b7b3eeab26e617
Joshua Jardenil <joshjardenil@live.com>
2016-07-05 19:42:23 -0700

    finished js side of website

--- random_quote_generator/js/main.js
+++ random_quote_generator/js/main.js
@@ -1,3 +1,10 @@
+// Goes generates a random quote from quotes.json
+function randomQuoteGenerator() {
+  var randomNumber = Math.floor(Math.random() * quotes.length);
+  return quotes[randomNumber];
+}
+
 $("document").ready(function(event) {
-  console.log("Hi friends");
+  var randomQuote = randomQuoteGenerator();
+  $("#quote-message").text(randomQuote);
 });
