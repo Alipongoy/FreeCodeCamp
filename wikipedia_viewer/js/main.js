54d2cd76ca3239ce2d03cd46a62cfd6c9ce2e52e
Joshua Jardenil <joshjardenil@live.com>
2016-07-19 18:00:42 -0700

    messing around with javascript

--- wikipedia_viewer/js/main.js
+++ wikipedia_viewer/js/main.js
@@ -11,6 +11,18 @@ function toggleTextSearch(){
   });
 }
 
+function toggleButtonFade() {
+  var bodySearch = $(".body-search-bar");
+
+  bodySearch.focus(function(){
+    console.log("shit is running");
+    $(".body-submit").animate({
+      backgroundColor: "#ffffff"
+    }, "slow");
+  });
+}
+
 $(document).ready(function(){
   toggleTextSearch();
+  toggleButtonFade();
 });
